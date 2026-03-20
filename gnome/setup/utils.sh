81436d6f9f785da84478c89dd496810c35a20e5c
Pedro Lucas <pedrolucaslcosta@gmail.com>
2026-03-20 17:01:16 -0300

    update gnome setup files

---
+++ gnome/setup/utils.sh
@@ -0,0 +1,4 @@
+
+
+dnf install fuse fuse-libs
+https://github.com/kem-a/AppManager?tab=readme-ov-file
---
+++ gnome/setup/visuals.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# ------------------------------------------------------------------------------
+# Visual Tweaks
+# ------------------------------------------------------------------------------
+
+gsettings set org.gnome.desktop.interface font-antialiasing grayscale
